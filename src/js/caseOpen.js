5375afe849bf7bcd84704ae5c6ea71738487305e
pupil <pupil@ivkhk.ee>
2024-03-19 10:41:04 +0200

    case json

--- src/js/caseOpen.js
+++ src/js/caseOpen.js
@@ -2,59 +2,184 @@ const caseData = {
     name: "Кейс Змеиный укус",
     items: {
         8000:{
-            "UMP-45 | Осциллятор": "cases/snakebite/1.png",
-            "SG 553 | Тяжёлый метал": "cases/snakebite/2.png",
-            "Револьвер R8 | Автосвалка": "cases/snakebite/3.png",
-            "Nova | На крыльях ветра": "cases/snakebite/4.png",
-            "M249 | O.S.I.P.R.": "cases/snakebite/5.png",
-            "Glock-18 | Прозрачный полимер": "cases/snakebite/6.png",
-            "CZ75-Auto | Настоящий змееяд": "cases/snakebite/7.png"
+            "UMP-45 | Осциллятор": {
+                price: 0.40,
+                img: "cases/snakebite/1.png"
+            },
+            "SG 553 | Тяжёлый метал": {
+                price: 0.44,
+                img: "cases/snakebite/2.png"
+            },
+            "Револьвер R8 | Автосвалка": {
+                price: 0.52,
+                img: "cases/snakebite/3.png"
+            },
+            "Nova | На крыльях ветра": {
+                price: 0.41,
+                img: "cases/snakebite/4.png"
+            },
+            "M249 | O.S.I.P.R.": {
+                price: 0.41,
+                img: "cases/snakebite/5.png"
+            },
+            "Glock-18 | Прозрачный полимер": {
+                price: 0.66,
+                img: "cases/snakebite/6.png"
+            },
+            "CZ75-Auto | Настоящий змееяд": {
+                price: 0.45,
+                img: "cases/snakebite/7.png"
+            },
         },
         1600:{
-            "P250 | Киберпанцирь": "cases/snakebite/8.png",
-            "Negev | dev_texture": "cases/snakebite/9.png",
-            "MAC-10 | Кнопкодав": "cases/snakebite/10.png",
-            "Desert Eagle | Стрелковая дисциплина": "cases/snakebite/11.png",
-            "AK-47 | Сланец": "cases/snakebite/12.png"
+            "P250 | Киберпанцирь": {
+                price: 1.63,
+                img: "cases/snakebite/8.png"
+            },
+            "Negev | dev_texture": {
+                price: 1.56,
+                img: "cases/snakebite/9.png"
+            },
+            "MAC-10 | Кнопкодав": {
+                price: 1.61,
+                img: "cases/snakebite/10.png"
+            },
+            "Desert Eagle | Стрелковая дисциплина": {
+                price: 3.99,
+                img: "cases/snakebite/11.png"
+            },
+            "AK-47 | Сланец": {
+                price: 13.26,
+                img: "cases/snakebite/12.png"
+            }
         },
         300:{
-            "XM1014 | Целую-обнимаю": "cases/snakebite/13.png",
-            "MP9 | Пищевая цепь": "cases/snakebite/14.png",
-            "Galil AR | Хроматическая аберрация": "cases/snakebite/15.png",
-            "Desert Eagle | Стрелковая дисциплина": "cases/snakebite/16.png"
+            "XM1014 | Целую-обнимаю": {
+                price: 10.00,
+                img: "cases/snakebite/13.png"
+            },
+            "MP9 | Пищевая цепь": {
+                price: 10.97,
+                img: "cases/snakebite/14.png"
+            },
+            "Galil AR | Хроматическая аберрация": {
+                price: 12.55,
+                img: "cases/snakebite/15.png"
+            }
         },
         95:{
-            "M4A4 | В живом цвете": "cases/snakebite/17.png",
-            "USP-S | Предатель": "cases/snakebite/18.png"
+            "USP-S | Предатель": {
+                price: 76.99,
+                img: "cases/snakebite/16.png"
+            },
+            "M4A4 | В живом цвете": {
+                price: 47.09,
+                img: "cases/snakebite/17.png"
+            }
         },
         5:{
-            "★ Спортивные перчатки | Мясник": "cases/gloves/1.png",
-            "★ Перчатки спецназа | Удар тигра": "cases/gloves/2.png",
-            "★ Водительские перчатки | Снежный барс": "cases/gloves/3.png",
-            "★ Перчатки спецназа | Мраморный градиент": "cases/gloves/4.png",
-            "★ Спортивные перчатки | Ноктс": "cases/gloves/5.png",
-            "★ Перчатки спецназа | Полевой агент": "cases/gloves/6.png",
-            "★ Водительские перчатки | Черный галстук": "cases/gloves/7.png",
-            "★ Спортивные перчатки | Кровавый шемах": "cases/gloves/8.png",
-            "★ Обмотки рук | ОСТОРОЖНО!": "cases/gloves/9.png",
-            "★ Мотоциклетные перчатки | Третья рота коммандо": "cases/gloves/10.png",
-            "★ Спортивные перчатки | Крупная добыча": "cases/gloves/11.png",
-            "★ Перчатки «Сломанный клык» | Без тормозов": "cases/gloves/12.png",
-            "★ Мотоциклетные перчатки | Бросаю дымовую": "cases/gloves/13.png",
-            "★ Перчатки спецназа | Капитан 3-го ранга": "cases/gloves/14.png",
-            "★ Мотоциклетные перчатки | Кровяное давление": "cases/gloves/15.png",
-            "★ Мотоциклетные перчатки | Финишная линия": "cases/gloves/16.png",
-            "★ Обмотки рук | Пустынный шемах": "cases/gloves/17.png",
-            "★ Перчатки «Сломанный клык» | Нефрит": "cases/gloves/18.png",
-            "★ Водительские перчатки | Резан Красный": "cases/gloves/19.png",
-            "★ Водительские перчатки | Королева ягуаров": "cases/gloves/20.png",
-            "★ Обмотки рук | Жираф": "cases/gloves/21.png",
-            "★ Перчатки «Сломанный клык» | Жёлтые полосы": "cases/gloves/22.png",
-            "★ Обмотки рук | Удав": "cases/gloves/23.png",
-            "★ Перчатки «Сломанный клык» | Остриё иглы": "cases/gloves/24.png"
+            "★ Спортивные перчатки | Мясник": {
+                price: 3531.83,
+                img: "cases/gloves/1.png"
+            },
+            "★ Перчатки спецназа | Удар тигра": {
+                price: 1431.74,
+                img: "cases/gloves/2.png"
+            },
+            "★ Водительские перчатки | Снежный барс": {
+                price: 1288.72,
+                img: "cases/gloves/3.png"
+            },
+            "★ Перчатки спецназа | Мраморный градиент": {
+                price: 1256.61,
+                img: "cases/gloves/4.png"
+            },
+            "★ Спортивные перчатки | Ноктс": {
+                price: 903.88,
+                img: "cases/gloves/5.png"
+            },
+            "★ Перчатки спецназа | Полевой агент": {
+                price: 881.41,
+                img: "cases/gloves/6.png"
+            },
+            "★ Водительские перчатки | Черный галстук": {
+                price: 778.98,
+                img: "cases/gloves/7.png"
+            },
+            "★ Спортивные перчатки | Кровавый шемах": {
+                price: 746.03,
+                img: "cases/gloves/8.png"
+            },
+            "★ Обмотки рук | ОСТОРОЖНО!": {
+                price: 553.42,
+                img: "cases/gloves/9.png"
+            },
+            "★ Мотоциклетные перчатки | Третья рота коммандо": {
+                price: 552.49,
+                img: "cases/gloves/10.png"
+            },
+            "★ Спортивные перчатки | Крупная добыча": {
+                price: 528.65,
+                img: "cases/gloves/11.png"
+            },
+            "★ Перчатки «Сломанный клык» | Без тормозов": {
+                price: 507.70,
+                img: "cases/gloves/12.png"
+            },
+            "★ Мотоциклетные перчатки | Бросаю дымовую": {
+                price: 500.12,
+                img: "cases/gloves/13.png"
+            },
+            "★ Перчатки спецназа | Капитан 3-го ранга": {
+                price: 483.64,
+                img: "cases/gloves/14.png"
+            },
+            "★ Мотоциклетные перчатки | Кровяное давление": {
+                price: 474.21,
+                img: "cases/gloves/15.png"
+            },
+            "★ Мотоциклетные перчатки | Финишная линия": {
+                price: 433.80,
+                img: "cases/gloves/16.png"
+            },
+            "★ Обмотки рук | Пустынный шемах": {
+                price: 346.11,
+                img: "cases/gloves/17.png"
+            },
+            "★ Перчатки «Сломанный клык» | Нефрит": {
+                price: 287.31,
+                img: "cases/gloves/18.png"
+            },
+            "★ Водительские перчатки | Резан Красный": {
+                price: 274.16,
+                img: "cases/gloves/19.png"
+            },
+            "★ Водительские перчатки | Королева ягуаров": {
+                price: 181.32,
+                img: "cases/gloves/20.png"
+            },
+            "★ Обмотки рук | Жираф": {
+                price: 176.51,
+                img: "cases/gloves/21.png"
+            },
+            "★ Перчатки «Сломанный клык» | Жёлтые полосы": {
+                price: 166.31,
+                img: "cases/gloves/22.png"
+            },
+            "★ Обмотки рук | Удав": {
+                price: 130.11,
+                img: "cases/gloves/23.png"
+            },
+            "★ Перчатки «Сломанный клык» | Остриё иглы": {
+                price: 118.24,
+                img: "cases/gloves/24.png"
+            }
         }
     },
     recived: {
-        "UMP-45 | Осциллятор": "cases/snakebite/1.png"
+        "UMP-45 | Осциллятор": {
+            price: 0.41,
+            img: "cases/snakebite/1.png"
+        }
     }
 }
