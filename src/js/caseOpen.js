9279aa6abf29babde03b2c0a99e9c19ac376e095
pupil <pupil@ivkhk.ee>
2024-03-19 10:17:53 +0200

    case json

---
+++ src/js/caseOpen.js
@@ -0,0 +1,60 @@
+const caseData = {
+    name: "Кейс Змеиный укус",
+    items: {
+        8000:{
+            "UMP-45 | Осциллятор": "cases/snakebite/1.png",
+            "SG 553 | Тяжёлый метал": "cases/snakebite/2.png",
+            "Револьвер R8 | Автосвалка": "cases/snakebite/3.png",
+            "Nova | На крыльях ветра": "cases/snakebite/4.png",
+            "M249 | O.S.I.P.R.": "cases/snakebite/5.png",
+            "Glock-18 | Прозрачный полимер": "cases/snakebite/6.png",
+            "CZ75-Auto | Настоящий змееяд": "cases/snakebite/7.png"
+        },
+        1600:{
+            "P250 | Киберпанцирь": "cases/snakebite/8.png",
+            "Negev | dev_texture": "cases/snakebite/9.png",
+            "MAC-10 | Кнопкодав": "cases/snakebite/10.png",
+            "Desert Eagle | Стрелковая дисциплина": "cases/snakebite/11.png",
+            "AK-47 | Сланец": "cases/snakebite/12.png"
+        },
+        300:{
+            "XM1014 | Целую-обнимаю": "cases/snakebite/13.png",
+            "MP9 | Пищевая цепь": "cases/snakebite/14.png",
+            "Galil AR | Хроматическая аберрация": "cases/snakebite/15.png",
+            "Desert Eagle | Стрелковая дисциплина": "cases/snakebite/16.png"
+        },
+        95:{
+            "M4A4 | В живом цвете": "cases/snakebite/17.png",
+            "USP-S | Предатель": "cases/snakebite/18.png"
+        },
+        5:{
+            "★ Спортивные перчатки | Мясник": "cases/gloves/1.png",
+            "★ Перчатки спецназа | Удар тигра": "cases/gloves/2.png",
+            "★ Водительские перчатки | Снежный барс": "cases/gloves/3.png",
+            "★ Перчатки спецназа | Мраморный градиент": "cases/gloves/4.png",
+            "★ Спортивные перчатки | Ноктс": "cases/gloves/5.png",
+            "★ Перчатки спецназа | Полевой агент": "cases/gloves/6.png",
+            "★ Водительские перчатки | Черный галстук": "cases/gloves/7.png",
+            "★ Спортивные перчатки | Кровавый шемах": "cases/gloves/8.png",
+            "★ Обмотки рук | ОСТОРОЖНО!": "cases/gloves/9.png",
+            "★ Мотоциклетные перчатки | Третья рота коммандо": "cases/gloves/10.png",
+            "★ Спортивные перчатки | Крупная добыча": "cases/gloves/11.png",
+            "★ Перчатки «Сломанный клык» | Без тормозов": "cases/gloves/12.png",
+            "★ Мотоциклетные перчатки | Бросаю дымовую": "cases/gloves/13.png",
+            "★ Перчатки спецназа | Капитан 3-го ранга": "cases/gloves/14.png",
+            "★ Мотоциклетные перчатки | Кровяное давление": "cases/gloves/15.png",
+            "★ Мотоциклетные перчатки | Финишная линия": "cases/gloves/16.png",
+            "★ Обмотки рук | Пустынный шемах": "cases/gloves/17.png",
+            "★ Перчатки «Сломанный клык» | Нефрит": "cases/gloves/18.png",
+            "★ Водительские перчатки | Резан Красный": "cases/gloves/19.png",
+            "★ Водительские перчатки | Королева ягуаров": "cases/gloves/20.png",
+            "★ Обмотки рук | Жираф": "cases/gloves/21.png",
+            "★ Перчатки «Сломанный клык» | Жёлтые полосы": "cases/gloves/22.png",
+            "★ Обмотки рук | Удав": "cases/gloves/23.png",
+            "★ Перчатки «Сломанный клык» | Остриё иглы": "cases/gloves/24.png"
+        }
+    },
+    recived: {
+        "UMP-45 | Осциллятор": "cases/snakebite/1.png"
+    }
+}
